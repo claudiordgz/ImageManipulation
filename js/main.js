eb5126c0e587c2d5712cf7d70d6ac75595f34270
claudiordgz <claudio.rodriguez@coxinc.com>
2015-09-04 15:02:00 -0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -239,32 +239,28 @@ function main() {
     }
 }
 
-$(document).one('ready', function() {
-    //main();
-
-    var get_data = function get_data () {
-        var api_url = 'http://lsp.powerathens.com/api/v1/station/now-playing/';
-        var ajax_obj = {
-            url: api_url,
-            data: {
-                callback: 'cmg.radioheader.lsploop_callbacks.callback0'
-            },
-            success: function() {
-                console.log('success');
-            },
-            method: 'GET',
-            error: function () {
-                console.log('Error polling LSP API:', arguments);
-            }
-        };
-        try {
-            $.ajax(ajax_obj);
-        } catch (err) {
-            console.log(err);
+var get_data = function get_data () {
+    var api_url = 'http://lsp.powerathens.com/api/v1/station/now-playing/';
+    var ajax_obj = {
+        url: api_url,
+        data: {
+            callback: 'cmg.radioheader.lsploop_callbacks.callback0'
+        },
+        success: function() {
+            console.log('success');
+        },
+        method: 'GET',
+        error: function () {
+            console.log('Error polling LSP API:', arguments);
         }
     };
+    try {
+        $.ajax(ajax_obj);
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-    get_data();
-
-
+$(document).one('ready', function() {
+    main();
 });
