0ac29155660cbb12e96ed03fb5ecd29fa3bd7ea7
claudiordgz <claudio.rodriguez@coxinc.com>
2015-09-23 10:01:17 -0400

    alpha test

---
+++ src/test/strategy/imageRecognition/graphics/util.js
@@ -0,0 +1,12 @@
+/*globals require, describe, it*/
+var expect    = require("chai").expect;
+var converter = require("../app/converter");
+
+describe("Color Code Converter", function() {
+    'use strict';
+    describe("Hex to RGB conversion", function() {
+        it("converts the basic colors", function() {
+
+        });
+    });
+});
