9441ad34e1b6cb63fb1fa1e72e9416d99b644e07
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-01 10:37:42 -0400

    the partitioning now returns parallelograms instead of vertex sets

--- src/lib/graphics/collision.js
+++ src/lib/graphics/collision.js
@@ -1,6 +1,5 @@
 /*globals module, console, require*/
-var Victor = require('victor');
-var parallelograms = require('graphics');
+var parallelograms = require('./graphics');
 
 /*  @function partitionSquareIntoFour
     Cuts the parallelogram into four pieces, origin 0,0 is the top-left corner
@@ -9,10 +8,10 @@ var parallelograms = require('graphics');
  */
 function partitionSquareIntoFour(parallelogram){
     'use strict';
-    var R1 = new parallelograms.ParallelogramVertexSet().fromVertices(0, parallelogram.width/2, 0, parallelogram.height/2),
-        R2 = new parallelograms.ParallelogramVertexSet().fromVertices(parallelogram.width/2, parallelogram.width,0, parallelogram.height/2),
-        R3 = new parallelograms.ParallelogramVertexSet().fromVertices(0,parallelogram.width/2, parallelogram.height/2, parallelogram.height),
-        R4 = new parallelograms.ParallelogramVertexSet().fromVertices(parallelogram.width/2, parallelogram.width, parallelogram.height/2, parallelogram.height);
+    var R1 = new parallelograms.Parallelogram().fromVertices(0, parallelogram.width/2, 0, parallelogram.height/2),
+        R2 = new parallelograms.Parallelogram().fromVertices(parallelogram.width/2, parallelogram.width,0, parallelogram.height/2),
+        R3 = new parallelograms.Parallelogram().fromVertices(0,parallelogram.width/2, parallelogram.height/2, parallelogram.height),
+        R4 = new parallelograms.Parallelogram().fromVertices(parallelogram.width/2, parallelogram.width, parallelogram.height/2, parallelogram.height);
     return {
         TopLeft: R1, TopRight: R2, LowerLeft: R3, LowerRight: R4
     };
@@ -31,14 +30,6 @@ function squareOverlap(faceBox) {
     concentricParallelogramCollision(subSquares, faceBox);
 }
 
-/*
-
- */
-function getVector(coordinatesA, coordinatesB) {
-    'use strict';
-    return new Victor(coordinatesB.x - coordinatesA.x, coordinatesB.y - coordinatesA.y);
-}
-
 /*  @function concentricParallelogramCollision
 
  */
@@ -47,23 +38,32 @@ function concentricParallelogramCollision(subSquares, faceBox) {
     assembleVectorsFromVertices(subSquares, faceBox);
 }
 
+
+function getVectorsPerSquare(squareVertices, targetVertex) {
+    'use strict';
+    console.log(squareVertices.OO.x, squareVertices.OO.y);
+    console.log(squareVertices.OA.x, squareVertices.OA.y);
+    console.log(squareVertices.OB.x, squareVertices.OB.y);
+    console.log(squareVertices.OC.x, squareVertices.OC.y);
+}
+
+/* @function assembleVectorsFromVertices
+ * Iterate through all the faceBox vertices and calculate the vectors between them.
+ *
+ */
 function assembleVectorsFromVertices(subSquares, faceBox) {
     'use strict';
     faceBox.recalculateVerticesWithOffset();
     for(var i=0; i!==faceBox.vertices.pMembers.length;++i){
         if(faceBox.vertices.hasOwnProperty(faceBox.vertices.pMembers[i])){
             var vertex = faceBox.vertices[faceBox.vertices.pMembers[i]];
-            //console.log(vertex);
             for(var key in subSquares){
                 if(subSquares.hasOwnProperty(key)){
-                    var a = 0;
+                    getVectorsPerSquare(subSquares[key].vertices, vertex);
                 }
-                //console.log('sub square: '+ subSquares[j].X1 + ' ' + subSquares[j].X2+ ' ' + subSquares[j].Y1+ ' ' + subSquares[j].Y2);
-                //console.log(getVector({x:subSquares[j].X1, y:subSquares[j].Y1},{x:vertex.x, y:vertex.y}));
             }
         }
     }
-
 }
 
 module.exports = {
