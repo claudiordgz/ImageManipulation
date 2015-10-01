b9c37508a1d0545f4defeca3644bd542ef9d053c
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-01 10:38:14 -0400

    the parallelogram can check if the point is inside it thank you very much

--- src/lib/graphics/objects/parallelogram.js
+++ src/lib/graphics/objects/parallelogram.js
@@ -1,5 +1,12 @@
 /*globals require, module*/
 var vertices = require('./parallelogramVertexSet');
+var vectorOps = require('../utilities/vectorOperations');
+
+/**
+ * @typedef {Object<string,Victor>} ParallelogramVectors
+ * @property {Victor} AB Vector from A to B
+ * @property {Victor} AD Vector from A to D
+ */
 
 /* @class Parallelogram with the following
  vertices
@@ -16,7 +23,46 @@ function Parallelogram() {
     'use strict';
     this.width = 0;
     this.height = 0;
+    this.vertices = new vertices.ParallelogramVertexSet();
+    this.__previousStateVertices = new vertices.ParallelogramVertexSet();
+    this.__isReady = false;
 }
+
+/** @function innerVectors
+ * Whatever vectors we need to do our algorithm to find a point inside
+ * the Parallelogram
+ * (0<AM⋅AB<AB⋅AB)∧(0<AM⋅AD<AD⋅AD)
+ * @return {ParallelogramVectors}
+ */
+Parallelogram.prototype.innerVectors = function(){
+    'use strict';
+    this.AB = vectorOps.getVector(this.vertices.A,  this.vertices.B);
+    this.AD = vectorOps.getVector(this.vertices.A,  this.vertices.D);
+    this.__isReady = true;
+};
+
+/** @function isPointInside
+ * Whatever vectors we need to do our algorithm to find a point inside
+ * the Parallelogram
+ * (0<AM⋅AB<AB⋅AB)∧(0<AM⋅AD<AD⋅AD)
+ * @param {Point} point The point we want to check if it is inside or not
+ * @return {Boolean} Whether it is inside or not
+ */
+Parallelogram.prototype.isPointInside = function(point){
+    'use strict';
+    var returnValue = false;
+    if(this.__isReady) {
+        var AM = vectorOps.getVector(this.vertices.A, point);
+        if(0 < AM.dot(this.AB) < this.AB.dot(this.AB) && 0 < AM.dot(this.AD) < this.AD.dot(this.AD)) {
+            returnValue = true;
+        }
+    } else {
+        this.innerVectors();
+        returnValue = this.isPointInside(point);
+    }
+    return returnValue;
+};
+
 
 /* @class Parallelogram with the following
  * @constructor
@@ -27,8 +73,8 @@ Parallelogram.prototype.fromWidthAndHeight = function(width, height) {
     'use strict';
     this.width = width;
     this.height = height;
-    this.vertices = new vertices.ParallelogramVertexSet().fromWidthAndHeight(width, height);
-    this.__previousStateVertices = new vertices.ParallelogramVertexSet().fromWidthAndHeight(width, height);
+    this.vertices.fromWidthAndHeight(width, height);
+    this.__previousStateVertices.fromWidthAndHeight(width, height);
     return this;
 };
 
@@ -44,8 +90,8 @@ Parallelogram.prototype.fromVertices = function(X1, X2, Y1, Y2) {
     'use strict';
     this.width = Math.abs(X2 - X1);
     this.height = Math.abs(Y2 - Y1);
-    this.vertices = new vertices.ParallelogramVertexSet().fromVertices(X1, X2, Y1, Y2);
-    this.__previousStateVertices = new vertices.ParallelogramVertexSet().fromVertices(X1, X2, Y1, Y2);
+    this.vertices.fromVertices(X1, X2, Y1, Y2);
+    this.__previousStateVertices.fromVertices(X1, X2, Y1, Y2);
     return this;
 };
 
