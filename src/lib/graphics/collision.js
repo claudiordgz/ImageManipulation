70cc6603e322da1cc1ecb23521903189d97fea1f
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-06 11:39:55 -0400

    moving typedef comments

--- src/lib/graphics/collision.js
+++ src/lib/graphics/collision.js
@@ -17,22 +17,6 @@ function partitionSquareIntoFour(parallelogram){
     };
 }
 
-/** @typedef {Object<string,string>} FaceBoxCollisionMap
- * The Parallelograms are as follows:
- *  @property {string} TopLeft
- *  @property {TopLeft.collisions.Vertex2D[]} Array of vertices from the faceBox in the TopLeft Quadrant
- *  @property {TopLeft.properties.Parallelogram} Sub Parallelogram properties
- *  @property {string} TopRight
- *  @property {TopRight.collisions.Vertex2D[]} Array of vertices from the faceBox in the TopRight Quadrant
- *  @property {TopRight.properties.Parallelogram} Sub Parallelogram properties
- *  @property {string} LowerLeft
- *  @property {LowerLeft.collisions.Vertex2D[]} Array of vertices from the faceBox in the LowerLeft Quadrant
- *  @property {LowerLeft.properties.Parallelogram} Sub Parallelogram properties
- *  @property {string} LowerRight
- *  @property {LowerRight.collisions.Vertex2D[]} Array of vertices from the faceBox in the LowerRight Quadrant
- *  @property {LowerRight.properties.Parallelogram} Sub Parallelogram properties
- */
-
 /*  @function squareOverlap
     Find where is the faceBox (the square containing ALL the faces)
     in the image, the offsets of the facebook are already applied.
--- src/lib/graphics/utilities/vectorOperations.js
+++ src/lib/graphics/utilities/vectorOperations.js
@@ -1,13 +1,6 @@
 /*globals require, module*/
 var Victor = require('victor');
 
-/**
- * @typedef Point
- * @type Object
- * @property {number} x The X Coordinate
- * @property {number} y The Y Coordinate
- */
-
 /** @function getVector
  * Utility to retrieve a vector between two points in a XY space
  * @param {Point} coordinatesA Origin location
