af592898a89a7d9a144a0758c19a8d481da290a8
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-12 17:22:02 -0400

    refactor: returning the stylesheet

--- src/lib/faceCentering/positioning.js
+++ src/lib/faceCentering/positioning.js
@@ -7,10 +7,64 @@ var switchQuadrantNameToIdentifier = {
     LowerLeft: 'D'
 };
 
-var switchCaseSelection = {
-    A: function(faceBox, faceBoxAnalysis) {
+var calculateAndAddPadding = {
+    TopLeft: function(anchorVertex, faceBox) {
         'use strict';
-        console.log(JSON.stringify(faceBoxAnalysis));
+        var PADDING_TOP_BOTTOM_PERCENT = 0.30,
+            PADDING_LEFT_RIGHT_PERCENT = 0.15,
+            faceBoxTopPadding = faceBox.height * PADDING_TOP_BOTTOM_PERCENT,
+            faceBoxLeftPadding = faceBox.width * PADDING_LEFT_RIGHT_PERCENT;
+        faceBoxTopPadding = faceBoxTopPadding > anchorVertex.y ? anchorVertex.y : faceBoxTopPadding;
+        faceBoxLeftPadding = faceBoxLeftPadding > anchorVertex.x ? anchorVertex.x : faceBoxLeftPadding;
+        return {
+            x: anchorVertex.x - faceBoxLeftPadding,
+            y: anchorVertex.y - faceBoxTopPadding,
+            xPadding: faceBoxLeftPadding,
+            yPadding: faceBoxTopPadding
+        };
+    }
+};
+
+var propertiesToCss = function(width, height, x, y) {
+    'use strict';
+    return {
+        'background-size': Math.round(width, 0).toString() + 'px ' + Math.round(height, 0).toString() + 'px',
+        'background-position': Math.round(x, 0).toString() + 'px ' + Math.round(y, 0).toString() + 'px'
+    };
+};
+
+var getWidthHeightFromAnchor = {
+    landscape: function(anchorPointAndPadding, faceBox)  {
+
+    },
+    portrait: function(anchorPointAndPadding, faceBox) {
+        'use strict';
+        var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+        if(faceBox.targetWidth < (faceBox.sourceWidth - anchorPointAndPadding.x)) {
+            aspectRatio = (faceBox.sourceWidth - anchorPointAndPadding.x) / faceBox.targetWidth;
+            positionYOffset = - (anchorPointAndPadding.y / aspectRatio);
+        } else {
+            aspectRatio = faceBox.sourceWidth / faceBox.targetWidth;
+            positionYOffset = anchorPointAndPadding.y === 0 ? 0 : - (anchorPointAndPadding.y / aspectRatio);
+        }
+        positionXOffset = 0;
+        newWidth = faceBox.sourceWidth / aspectRatio;
+        newHeight = faceBox.sourceHeight / aspectRatio;
+        return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+    },
+    square: function(anchorPointAndPadding, faceBox) {
+
+    }
+};
+
+
+
+var switchCaseSelection = {
+    A: function(faceBox) {
+        'use strict';
+        var newAnchor = calculateAndAddPadding.TopLeft(faceBox.vertices.A, faceBox);
+        newAnchor = getWidthHeightFromAnchor[faceBox.getOrientation()](newAnchor, faceBox);
+        return newAnchor;
     },
     B: function(faceBox, faceBoxAnalysis){
         'use strict';
@@ -84,11 +138,14 @@ function selectAnchorPoint(faceBox, faceBoxAnalysis) {
     priorities.map(function(key){
         Quadrants += switchQuadrantNameToIdentifier[key[0]];
     });
+    console.log(priorities);
+    var style;
     if(priorities.length === 4) {
-        switchCaseSelection.ALL(faceBox, faceBoxAnalysis, priorities);
+        style = switchCaseSelection.ALL(faceBox, faceBoxAnalysis, priorities);
     } else {
-        switchCaseSelection[Quadrants](faceBox, faceBoxAnalysis, priorities);
+        style = switchCaseSelection[Quadrants](faceBox, faceBoxAnalysis, priorities);
     }
+    return style;
 }
 
 module.exports = {
--- src/lib/faceCentering/test/positioning.js
+++ src/lib/faceCentering/test/positioning.js
@@ -2,46 +2,57 @@
 var chai    = require("chai");
 var testbed = require("./testbeds/faceBoxAndFaceBoxAnalysis");
 var positioning = require('../positioning');
+var graphics = require('graphics');
 
 describe("Selecting an anchor point", function() {
     'use strict';
     /** FaceContainer
      * width, height, offsetX, offsetY, imageWidth, imageHeight, containerWidth, containerHeight*/
     describe("selectAnchorPoint from area percentages per quadrant", function() {
+        var baseTest = function(testData) {
+            var testFaceBox = new graphics.FaceContainer(testData.faceBox.width, testData.faceBox.height, testData.faceBox.offsetX,
+                testData.faceBox.offsetY, testData.faceBox.sourceWidth, testData.faceBox.sourceHeight,
+                testData.faceBox.targetWidth, testData.faceBox.targetHeight);
+            testFaceBox.recalculateVerticesWithOffset();
+            positioning.selectAnchorPoint(
+                testFaceBox,
+                testData.faceBoxAnalysis);
+        };
+
         describe("Test for rFB292x326Image0", function() {
             it("Orders the areas by highest and picks a ", function () {
                 var testData = testbed.rFB292x326Image0;
-                positioning.selectAnchorPoint(testData.faceBox, testData.faceBoxAnalysis);
+                baseTest(testData);
             });
         });
         describe("Test for rFB166x250Image1", function() {
             it("Orders the areas by highest and picks a ", function () {
                 var testData = testbed.rFB166x250Image1;
-                positioning.selectAnchorPoint(testData.faceBox, testData.faceBoxAnalysis);
+                baseTest(testData);
             });
         });
         describe("Test for rFB400x268Image3", function() {
             it("Orders the areas by highest and picks a ", function () {
                 var testData = testbed.rFB400x268Image3;
-                positioning.selectAnchorPoint(testData.faceBox, testData.faceBoxAnalysis);
+                baseTest(testData);
             });
         });
         describe("Test for rFB400x272Image5", function() {
             it("Orders the areas by highest and picks a ", function () {
                 var testData = testbed.rFB400x272Image5;
-                positioning.selectAnchorPoint(testData.faceBox, testData.faceBoxAnalysis);
+                baseTest(testData);
             });
         });
         describe("Test for rFB250x206Image4", function() {
             it("Orders the areas by highest and picks a ", function () {
                 var testData = testbed.rFB250x206Image4;
-                positioning.selectAnchorPoint(testData.faceBox, testData.faceBoxAnalysis);
+                baseTest(testData);
             });
         });
         describe("Test for rFB250x248Image7", function() {
             it("Orders the areas by highest and picks a ", function () {
                 var testData = testbed.rFB250x248Image7;
-                positioning.selectAnchorPoint(testData.faceBox, testData.faceBoxAnalysis);
+                baseTest(testData);
             });
         });
     });
