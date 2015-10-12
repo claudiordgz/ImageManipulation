ec09dc19afa69467fd1ca29ff7cddb9f7e976b69
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-12 17:21:17 -0400

    feat: next button to browse more images

--- src/app/main.js
+++ src/app/main.js
@@ -2,14 +2,27 @@
 var testbed = require('faces/testbed/main');
 var imageProcessing = require('faces/strategy/main');
 
-function main() {
+window.items = testbed.fromDirectory();
+window.index = 0;
+
+function main(images) {
     'use strict';
-    var items = testbed.fromDirectorySmallSubset();
-    //var items = testbed.fromDirectory();
-    imageProcessing.processImages(items);
+    if(images.length > 0){
+        imageProcessing.processImages(images);
+    }
 }
 
 $(document).one('ready', function() {
     'use strict';
-    main();
+    main(window.items.slice(window.index,2));
+
+    $(document).on("click", '.nextButton',function(e){
+        $(".mass").html('');
+        if(window.index === window.items.length - 1) {
+            window.index = 0;
+        } else {
+            window.index += 2;
+        }
+        main(window.items.slice(window.index,window.index+2));
+    });
 });
