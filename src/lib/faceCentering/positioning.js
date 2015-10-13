907b53721ed8bb080148b959bdbec702bd34cd06
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-13 19:54:07 -0400

    feat: extra anchor logic

--- src/lib/faceCentering/positioning.js
+++ src/lib/faceCentering/positioning.js
@@ -1,5 +1,6 @@
 /*globals console, module, require*/
 var padding = require('./padding');
+var graphics = require('graphics');
 
 var switchQuadrantNameToIdentifier = {
     TopLeft: 'A',
@@ -10,15 +11,129 @@ var switchQuadrantNameToIdentifier = {
 
 var propertiesToCss = function(width, height, x, y) {
     'use strict';
+    var xString = x !== 'center' ? Math.round(x, 0).toString() + 'px' : x;
+    var yString = y !== 'center' ? Math.round(y, 0).toString() + 'px' : y;
+
     return {
         'background-size': Math.round(width, 0).toString() + 'px ' + Math.round(height, 0).toString() + 'px',
-        'background-position': Math.round(x, 0).toString() + 'px ' + Math.round(y, 0).toString() + 'px'
+        'background-position': xString + ' ' + yString
     };
 };
 
 var getWidthHeightFromAnchor = {
-    landscape: function(anchorPointAndPadding, faceBox)  {
-
+    landscape: {
+        TopLeft: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetHeight < (faceBox.sourceHeight - anchorPointAndPadding.y)) {
+                aspectRatio = (faceBox.sourceHeight - anchorPointAndPadding.y) / faceBox.targetHeight;
+                positionYOffset = anchorPointAndPadding.y === 0 ? 0 : - (anchorPointAndPadding.y / aspectRatio);
+            } else {
+                aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+                positionYOffset = 0;
+            }
+            positionXOffset = 0;
+            if(anchorPointAndPadding.x > (faceBox.sourceWidth / 4)) {
+                positionXOffset = - (anchorPointAndPadding.x / 2);
+            }
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        TopRight: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetHeight < (faceBox.sourceHeight - anchorPointAndPadding.y)) {
+                aspectRatio = (faceBox.sourceHeight - anchorPointAndPadding.y) / faceBox.targetHeight;
+                positionYOffset = anchorPointAndPadding.y === 0 ? 0 : - (anchorPointAndPadding.y / aspectRatio);
+            } else {
+                aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+                positionYOffset = 0;
+            }
+            positionXOffset = 0;
+            if((faceBox.sourceWidth - anchorPointAndPadding.x) > (faceBox.sourceWidth / 4)) {
+                positionXOffset = - ((faceBox.sourceWidth - anchorPointAndPadding.x) / 2);
+            }
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        LowerLeft: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetHeight < anchorPointAndPadding.y) {
+                aspectRatio = anchorPointAndPadding.y / faceBox.targetHeight;
+                positionYOffset = (faceBox.sourceHeight - anchorPointAndPadding.y) === 0 ? 0 : - ((faceBox.sourceHeight - anchorPointAndPadding.y) / aspectRatio);
+            } else {
+                aspectRatio = anchorPointAndPadding.y / faceBox.targetHeight;
+                positionYOffset = 0;
+            }
+            positionXOffset = 0;
+            if(anchorPointAndPadding.x > (faceBox.sourceWidth / 4)) {
+                positionXOffset = - (anchorPointAndPadding.x / 2);
+            }
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        LowerRight: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetHeight < (faceBox.sourceHeight - anchorPointAndPadding.y)) {
+                aspectRatio = (faceBox.sourceHeight - anchorPointAndPadding.y) / faceBox.targetHeight;
+                positionYOffset = anchorPointAndPadding.y === 0 ? 0 : - (anchorPointAndPadding.y / aspectRatio);
+            } else {
+                aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+                positionYOffset = 0;
+            }
+            positionXOffset = 0;
+            if((faceBox.sourceWidth - anchorPointAndPadding.x) > (faceBox.sourceWidth / 4)) {
+                positionXOffset = - ((faceBox.sourceWidth - anchorPointAndPadding.x) / 2);
+            }
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterTop: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+            var positionYOffset = 0;
+            positionXOffset = 'center';
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterLeft: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+            var positionYOffset = 'center';
+            positionXOffset = 0;
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterRight: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+            var positionYOffset = 'center';
+            positionXOffset = 0;
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterBottom: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceHeight / faceBox.targetHeight;
+            var positionYOffset = 0;
+            positionXOffset = 'center';
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        }
     },
     portrait: {
         TopLeft: function(anchorPointAndPadding, faceBox) {
@@ -35,61 +150,200 @@ var getWidthHeightFromAnchor = {
             newWidth = faceBox.sourceWidth / aspectRatio;
             newHeight = faceBox.sourceHeight / aspectRatio;
             return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        TopRight: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetWidth < anchorPointAndPadding.x) {
+                aspectRatio = anchorPointAndPadding.x / faceBox.targetWidth;
+                positionYOffset = - (anchorPointAndPadding.y / aspectRatio);
+            } else {
+                aspectRatio = anchorPointAndPadding.x / faceBox.targetWidth;
+                positionYOffset = anchorPointAndPadding.y === 0 ? 0 : - (anchorPointAndPadding.y / aspectRatio);
+            }
+            positionXOffset = -((faceBox.sourceWidth - anchorPointAndPadding.x) / aspectRatio);
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        LowerLeft: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetWidth < (faceBox.sourceWidth - anchorPointAndPadding.x)) {
+                aspectRatio = (faceBox.sourceWidth - anchorPointAndPadding.x) / faceBox.targetWidth;
+                positionYOffset = - (faceBox.sourceHeight - anchorPointAndPadding.y / aspectRatio);
+            } else {
+                aspectRatio = faceBox.sourceWidth / faceBox.targetWidth;
+                positionYOffset = (faceBox.sourceHeight - anchorPointAndPadding.y) === 0 ? 0 : - ((faceBox.sourceHeight - anchorPointAndPadding.y) / aspectRatio);
+            }
+            positionXOffset = 0;
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        LowerRight: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var aspectRatio, newWidth, newHeight, positionXOffset, positionYOffset;
+            if(faceBox.targetWidth < anchorPointAndPadding.x) {
+                aspectRatio = anchorPointAndPadding.x / faceBox.targetWidth;
+                positionYOffset = - ((faceBox.sourceHeight - anchorPointAndPadding.y) / aspectRatio);
+            } else {
+                aspectRatio = anchorPointAndPadding.x / faceBox.targetWidth;
+                positionYOffset = (faceBox.sourceHeight - anchorPointAndPadding.y) === 0 ? 0 : - ((faceBox.sourceHeight - anchorPointAndPadding.y) / aspectRatio);
+            }
+            positionXOffset = -((faceBox.sourceWidth - anchorPointAndPadding.x) / aspectRatio);
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterTop: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceWidth / faceBox.targetWidth;
+            var positionYOffset = anchorPointAndPadding.y === 0 ? 0 : - (anchorPointAndPadding.y / aspectRatio);
+            positionXOffset = 'center';
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterLeft: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceWidth / faceBox.targetWidth;
+            var positionYOffset = 'center';
+            positionXOffset = 0;
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterRight: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceWidth / faceBox.targetWidth;
+            var positionYOffset = 'center';
+            positionXOffset = 0;
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
+        },
+        CenterBottom: function(anchorPointAndPadding, faceBox) {
+            'use strict';
+            var newWidth, newHeight, positionXOffset;
+            var aspectRatio = faceBox.sourceWidth / faceBox.targetWidth;
+            var positionYOffset = (faceBox.sourceHeight - anchorPointAndPadding.y) === 0 ? 0 : - ((faceBox.sourceHeight - anchorPointAndPadding.y) / aspectRatio);
+            positionXOffset = 'center';
+            newWidth = faceBox.sourceWidth / aspectRatio;
+            newHeight = faceBox.sourceHeight / aspectRatio;
+            return propertiesToCss(newWidth, newHeight, positionXOffset, positionYOffset);
         }
-    },
-    square: function(anchorPointAndPadding, faceBox) {
-
     }
 };
 
-
-
 var switchCaseSelection = {
+    forceDefaultOrientation: function(orientation) {
+        'use strict';
+        if(orientation === 'square') {
+            return 'landscape';
+        }
+        return orientation;
+    },
     A: function(faceBox) {
         'use strict';
         var newAnchor = padding.calculateAndAddPadding.TopLeft(faceBox.vertices.A, faceBox);
-        newAnchor = getWidthHeightFromAnchor[faceBox.getOrientation()].TopLeft(newAnchor, faceBox);
-        console.log(newAnchor);
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].TopLeft(newAnchor, faceBox);
         return newAnchor;
     },
-    B: function(faceBox, faceBoxAnalysis){
+    B: function(faceBox){
         'use strict';
         var newAnchor = padding.calculateAndAddPadding.TopRight(faceBox.vertices.B, faceBox);
-        newAnchor = getWidthHeightFromAnchor[faceBox.getOrientation()].TopRight(newAnchor, faceBox);
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].TopRight(newAnchor, faceBox);
         return newAnchor;
     },
-    C: function(faceBox, faceBoxAnalysis){
+    C: function(faceBox){
         'use strict';
-
+        var newAnchor = padding.calculateAndAddPadding.LowerRight(faceBox.vertices.C, faceBox);
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].LowerRight(newAnchor, faceBox);
+        return newAnchor;
     },
-    D: function(faceBox, faceBoxAnalysis){
+    D: function(faceBox){
         'use strict';
-
+        var newAnchor = padding.calculateAndAddPadding.LowerRight(faceBox.vertices.D, faceBox);
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].LowerLeft(newAnchor, faceBox);
+        return newAnchor;
     },
     /* Top Quadrants Left to Right */
     AB: function(faceBox, faceBoxAnalysis){
         'use strict';
-
+        var newAnchor;
+        if(faceBoxAnalysis.TopLeft.percent > 0.75) {
+            newAnchor = padding.calculateAndAddPadding.TopLeft(faceBox.vertices.A, faceBox);
+        } else {
+            var anchorPoint = new graphics.Vertex2D(faceBox.vertices.B.x - faceBox.vertices.A.x, faceBox.vertices.A.y);
+            newAnchor = padding.calculateAndAddPadding.CenterTop(anchorPoint, faceBox);
+        }
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].CenterTop(newAnchor, faceBox);
+        return newAnchor;
     },
     /* Bottom Quadrants Left to Right */
     DC: function(faceBox, faceBoxAnalysis){
         'use strict';
-
+        var newAnchor;
+        if(faceBoxAnalysis.LowerLeft.percent > 0.75) {
+            newAnchor = padding.calculateAndAddPadding.LowerLeft(faceBox.vertices.D, faceBox);
+        } else {
+            var anchorPoint = new graphics.Vertex2D(faceBox.vertices.C.x - faceBox.vertices.D.x, faceBox.vertices.D.y);
+            newAnchor = padding.calculateAndAddPadding.CenterBottom(anchorPoint, faceBox);
+        }
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].CenterBottom(newAnchor, faceBox);
+        return newAnchor;
     },
     /* Top Quadrants Right to Left */
     BA: function(faceBox, faceBoxAnalysis){
         'use strict';
-
+        var newAnchor;
+        if(faceBoxAnalysis.TopRight.percent > 0.75) {
+            newAnchor = padding.calculateAndAddPadding.TopRight(faceBox.vertices.B, faceBox);
+        } else {
+            var anchorPoint = new graphics.Vertex2D(faceBox.vertices.B.x - faceBox.vertices.A.x, faceBox.vertices.A.y);
+            newAnchor = padding.calculateAndAddPadding.CenterTop(anchorPoint, faceBox);
+        }
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].CenterTop(newAnchor, faceBox);
+        return newAnchor;
     },
     /* Bottom Quadrants Right to Left */
     CD: function(faceBox, faceBoxAnalysis){
         'use strict';
-
+        var newAnchor;
+        if(faceBoxAnalysis.LowerRight.percent > 0.75) {
+            newAnchor = padding.calculateAndAddPadding.LowerRight(faceBox.vertices.C, faceBox);
+        } else {
+            var anchorPoint = new graphics.Vertex2D(faceBox.vertices.C.x - faceBox.vertices.D.x, faceBox.vertices.D.y);
+            newAnchor = padding.calculateAndAddPadding.CenterBottom(anchorPoint, faceBox);
+        }
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].CenterBottom(newAnchor, faceBox);
+        return newAnchor;
     },
     /* Left Quadrants Top to Bottom  */
     AD: function(faceBox, faceBoxAnalysis){
         'use strict';
-
+        var newAnchor;
+        if(faceBoxAnalysis.LowerRight.percent > 0.75) {
+            newAnchor = padding.calculateAndAddPadding.LowerRight(faceBox.vertices.C, faceBox);
+        } else {
+            var anchorPoint = new graphics.Vertex2D(faceBox.vertices.C.x - faceBox.vertices.D.x, faceBox.vertices.D.y);
+            newAnchor = padding.calculateAndAddPadding.CenterBottom(anchorPoint, faceBox);
+        }
+        var orientation = this.forceDefaultOrientation(faceBox.getOrientation());
+        newAnchor = getWidthHeightFromAnchor[orientation].CenterBottom(newAnchor, faceBox);
+        return newAnchor;
     },
     /* Right Quadrants Top to Bottom  */
     BC: function(faceBox, faceBoxAnalysis){
@@ -126,13 +380,14 @@ function selectAnchorPoint(faceBox, faceBoxAnalysis) {
     priorities.map(function(key){
         Quadrants += switchQuadrantNameToIdentifier[key[0]];
     });
-    console.log(priorities);
+    console.log(Quadrants);
     var style;
     if(priorities.length === 4) {
         style = switchCaseSelection.ALL(faceBox, faceBoxAnalysis, priorities);
     } else {
         style = switchCaseSelection[Quadrants](faceBox, faceBoxAnalysis, priorities);
     }
+    console.log(style);
     return style;
 }
 
