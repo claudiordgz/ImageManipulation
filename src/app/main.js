e3c0a2b055ff0ab313812feea2a97130fde1080a
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-13 03:33:31 -0400

    bug: styles where being reused, global sheet being created and removed and indexes being switched double fixes this

--- src/app/main.js
+++ src/app/main.js
@@ -1,20 +1,21 @@
-/*globals require, $*/
+/*globals require, $, cssjs*/
 var testbed = require('faces/testbed/main');
 var imageProcessing = require('faces/strategy/main');
 
 window.items = testbed.fromDirectory();
 window.index = 0;
+window.sheet = cssjs.newSheet();
 
-function main(images) {
+function main(images, sheet, alterIndex) {
     'use strict';
     if(images.length > 0){
-        imageProcessing.processImages(images);
+        imageProcessing.processImages(images, sheet, alterIndex);
     }
 }
 
 $(document).one('ready', function() {
     'use strict';
-    main(window.items.slice(window.index,2));
+    main(window.items.slice(window.index,2), window.sheet, window.index);
 
     $(document).on("click", '.nextButton',function(e){
         $(".mass").html('');
@@ -23,6 +24,8 @@ $(document).one('ready', function() {
         } else {
             window.index += 2;
         }
-        main(window.items.slice(window.index,window.index+2));
+        window.sheet.remove();
+        window.sheet = cssjs.newSheet();
+        main(window.items.slice(window.index,window.index+2), window.sheet, window.index);
     });
 });
--- src/app/strategy/main.js
+++ src/app/strategy/main.js
@@ -6,7 +6,7 @@ var policies = require('faces/strategy/policies');
 // cm_mobHeader_artist_image - The circle div that will contain the image
 // cm_mobHeader_artist-overlay--style - We will put everything related to the background of artist_overlay in here
 // cm_mobHeader_artist-image--style - We will put everything related to background of artist_image in here
-function defaultPolicy(index, element, imgUrl, policy) {
+function defaultPolicy(index, element, imgUrl, cssSheet, policy) {
     'use strict';
     var ovalBackgroundClassName = util.format('cm_mobHeader_artist-image--style-{0}', index);
 
@@ -16,8 +16,7 @@ function defaultPolicy(index, element, imgUrl, policy) {
         'background-color': 'transparent'
     };
 
-    var sheet = cssjs.newSheet();
-    sheet.selector('.' + overlayClassName, overlayBackground);
+    cssSheet.selector('.' + overlayClassName, overlayBackground);
 
     var overlayImg = new Image();
     overlayImg.element = element;
@@ -31,7 +30,7 @@ function defaultPolicy(index, element, imgUrl, policy) {
 
     ovalImg.element = element;
     ovalImg.className = ovalBackgroundClassName;
-    ovalImg.css = sheet;
+    ovalImg.css = cssSheet;
     ovalImg.imageSrc = imgUrl;
     ovalImg.policy = policy;
     ovalImg.self = ovalImg;
@@ -42,7 +41,7 @@ function defaultPolicy(index, element, imgUrl, policy) {
                 container.containerProperties = util.getProperties(container[0]);
             var style = this.policy(this.self, container, this.imageSrc, this.width, this.height, this.className, this.css);
             if(style !== undefined) {
-                sheet.selector('.' + this.className, style);
+                this.css.selector('.' + this.className, style);
             }
             container.addClass(this.className);
         }
@@ -50,25 +49,27 @@ function defaultPolicy(index, element, imgUrl, policy) {
     ovalImg.src = imgUrl;
 }
 
-function appendElement(index, imageUrl, row, element, policy, subPolicy) {
+function appendElement(index, imageUrl, row, element, cssSheet, policy, subPolicy) {
     'use strict';
     var card = element({
         'songName': 'Song Name',
         'artistName': 'Artist Name'
     });
-    policy(index, card, imageUrl, subPolicy);
+    policy(index, card, imageUrl, cssSheet, subPolicy);
     $(row).append(card);
 }
 
-function processImages(images){
+function processImages(images, cssSheet, beginIndex){
     'use strict';
+    var alterIndex = beginIndex;
     for(var i = 0; i !== images.length; ++i) {
         var row = ich.elRow();
-        var indexString = i.toString();
-        appendElement(indexString+'-a', images[i], row, ich.element, defaultPolicy, policies.trackingJs);
-        appendElement(indexString+'-b', images[i], row, ich.element, defaultPolicy, policies.widthHeightPositioning);
-        appendElement(indexString+'-c', images[i], row, ich.element, defaultPolicy, policies.widthHeightPositioning);
+        var indexString = alterIndex.toString();
+        appendElement(indexString+'-a', images[i], row, ich.element, cssSheet, defaultPolicy, policies.trackingJs);
+        appendElement(indexString+'-b', images[i], row, ich.element, cssSheet, defaultPolicy, policies.widthHeightPositioning);
+        appendElement(indexString+'-c', images[i], row, ich.element, cssSheet, defaultPolicy, policies.widthHeightPositioning);
         $(".mass").append(row);
+        alterIndex++;
     }
 }
 
