ecd110d1dae0b5a3378700c600d79a442903bb4f
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-12 22:48:20 -0400

    feat: current strategy

--- src/app/strategy/widthHeightPositioning/main.js
+++ src/app/strategy/widthHeightPositioning/main.js
@@ -3,9 +3,16 @@ var util = require('faceCentering/util/util');
 
 function currentPolicy(imageElement, imageContainer, imgUrl, width, height, imgClass, cssStyleSheet) {
     'use strict';
-    return {
-        'background-image': util.format('url(\'{0}\')', imgUrl)
-    };
+    var extraProperty = {};
+    if(width > height) {
+        extraProperty['background-size'] = 'auto 100%';
+    } else if(width < height) {
+        extraProperty['background-size'] = '100% auto';
+    } else {
+        extraProperty['background-size'] = '100% auto';
+    }
+    extraProperty['background-image'] =  util.format('url(\'{0}\')', imgUrl);
+    return extraProperty;
 }
 
 module.exports = {
