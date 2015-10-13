0f748709547609b0c583cb8d9053bf26a89662a2
claudiordgz <claudio.rodriguez@coxinc.com>
2015-10-13 01:18:23 -0400

    feat: finished padding for all anchors

--- src/lib/faceCentering/padding.js
+++ src/lib/faceCentering/padding.js
@@ -3,7 +3,7 @@
 var calculateAndAddPadding = {
     PADDING_TOP_BOTTOM_PERCENT: 0.30,
     PADDING_LEFT_RIGHT_PERCENT: 0.15,
-    applyPadding: function(anchorVertex, faceBox, boundaryX, boundaryY, operationX, operationY) {
+    applyPaddingCorner: function(anchorVertex, faceBox, boundaryX, boundaryY, operationX, operationY) {
         'use strict';
         var faceBoxTopOrBottomPadding = faceBox.height * this.PADDING_TOP_BOTTOM_PERCENT,
             faceBoxLeftOrRightPadding = faceBox.width * this.PADDING_LEFT_RIGHT_PERCENT;
@@ -16,32 +16,61 @@ var calculateAndAddPadding = {
             yPadding: faceBoxTopOrBottomPadding
         };
     },
-    TopLeft: function(anchorVertex, faceBox) {
-        'use strict';
-        return this.applyPadding(anchorVertex, faceBox, this.boundaryXLeft, this.boundaryYTop, this.minus, this.minus);
-    },
-    TopRight: function(anchorVertex, faceBox) {
-        'use strict';
-        return this.applyPadding(anchorVertex, faceBox, this.boundaryXRight, this.boundaryYTop, this.add, this.minus);
-    },
-    LowerLeft: function(anchorVertex, faceBox) {
-        'use strict';
-        return this.applyPadding(anchorVertex, faceBox, this.boundaryXLeft, this.boundaryYBottom, this.minus, this.add);
-    },
-    LowerRight: function(anchorVertex, faceBox) {
-        'use strict';
-        return this.applyPadding(anchorVertex, faceBox, this.boundaryXRight, this.boundaryYBottom, this.add, this.add);
+    applyPaddingCenterOfEdge: {
+        TopBottom: function(anchorVertex, faceBox, boundaryY, operationY) {
+            'use strict';
+            var faceBoxTopOrBottomPadding = faceBox.height * this.PADDING_TOP_BOTTOM_PERCENT;
+            faceBoxTopOrBottomPadding = boundaryY(faceBoxTopOrBottomPadding, anchorVertex.y, faceBox.sourceHeight)? anchorVertex.y : faceBoxTopOrBottomPadding;
+            return {
+                x: anchorVertex.x,
+                y: operationY(anchorVertex.y, faceBoxTopOrBottomPadding),
+                xPadding: 0,
+                yPadding: faceBoxTopOrBottomPadding
+            };
+        },
+        LeftRight: function(anchorVertex, faceBox, boundaryX, operationX) {
+            'use strict';
+            var faceBoxLeftOrRightPadding = faceBox.width * this.PADDING_LEFT_RIGHT_PERCENT;
+            faceBoxLeftOrRightPadding = boundaryX(faceBoxLeftOrRightPadding, anchorVertex.x, faceBox.sourceWidth) ? anchorVertex.x : faceBoxLeftOrRightPadding;
+            return {
+                x: operationX(anchorVertex.x, faceBoxLeftOrRightPadding),
+                y: anchorVertex.y,
+                xPadding: faceBoxLeftOrRightPadding,
+                yPadding: 0
+            };
+        }
     },
     CenterTop: function(anchorVertex, faceBox) {
         'use strict';
-        var padding = this.applyPadding(anchorVertex, faceBox, this.boundaryXRight, this.boundaryYBottom, this.add, this.add);
-        padding.x = anchorVertex.x;
-        padding.xPadding = 0;
-        return padding;
+        return this.applyPaddingCenterOfEdge.TopBottom(anchorVertex, faceBox, this.boundaryYTop, this.minus);
     },
     CenterLeft: function(anchorVertex, faceBox) {
         'use strict';
-        return this.applyPadding(anchorVertex, faceBox, this.boundaryXRight, this.boundaryYBottom, this.add, this.add);
+        return this.applyPaddingCenterOfEdge.LeftRight(anchorVertex, faceBox, this.boundaryXLeft, this.minus);
+    },
+    CenterBottom: function(anchorVertex, faceBox) {
+        'use strict';
+        return this.applyPaddingCenterOfEdge.TopBottom(anchorVertex, faceBox, this.boundaryYBottom, this.add);
+    },
+    CenterRight: function(anchorVertex, faceBox) {
+        'use strict';
+        return this.applyPaddingCenterOfEdge.LeftRight(anchorVertex, faceBox, this.boundaryXRight, this.add);
+    },
+    TopLeft: function(anchorVertex, faceBox) {
+        'use strict';
+        return this.applyPaddingCorner(anchorVertex, faceBox, this.boundaryXLeft, this.boundaryYTop, this.minus, this.minus);
+    },
+    TopRight: function(anchorVertex, faceBox) {
+        'use strict';
+        return this.applyPaddingCorner(anchorVertex, faceBox, this.boundaryXRight, this.boundaryYTop, this.add, this.minus);
+    },
+    LowerLeft: function(anchorVertex, faceBox) {
+        'use strict';
+        return this.applyPaddingCorner(anchorVertex, faceBox, this.boundaryXLeft, this.boundaryYBottom, this.minus, this.add);
+    },
+    LowerRight: function(anchorVertex, faceBox) {
+        'use strict';
+        return this.applyPaddingCorner(anchorVertex, faceBox, this.boundaryXRight, this.boundaryYBottom, this.add, this.add);
     },
     minus: function(a, b){
         'use strict';
